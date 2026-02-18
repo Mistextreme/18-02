49cacbb8d75639344ab2e40a77d563a1f6020565
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2026-02-18 21:48:11 +0000

    Upload: fxmanifest.lua

---
+++ sovex_garage/fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Sovex'
+description 'sovex_garage - Garage system for QBCore'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@qb-core/shared/locale.lua',
+    'shared/config.lua',
+    'shared/locales.lua',
+}
+
+client_scripts {
+    'client/main.lua',
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua',
+}
+ui_page 'ui/index.html'
+
+files {
+    'ui/index.html',
+    'ui/style.css',
+    'ui/app.js'
+}
+escrow_ignore {
+    'config.lua',
+    'locales.lua'
+}
+lua54 'yes'
+
+dependency '/assetpacks'
